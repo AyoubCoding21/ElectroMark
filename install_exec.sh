83b2e3a6bafa0ae8dd1d817d30a916364fb3293b
AyoubCoding21 <120122307+AyoubCoding21@users.noreply.github.com>
2022-12-18 10:44:26 +0100

    Update install_exec.sh

--- install_exec.sh
+++ install_exec.sh
@@ -15,7 +15,7 @@ echo -e "\e[1;32m${bold}Compilling and executing...${normal} \e[0m"
 clang++ ./src/main.cpp -o ./build/ElectroMark -mfp64 -mlong-double-128 -DFREEGLUT_STATIC -L/usr/lib64 -lGLEW -lGL -lX11 -lGLU -lglut
 if [ -f ./build/ElectroMark ]
 then
-	./build/ElectroMark
+	chmod u+x ./build/ElectroMark && ./build/ElectroMark
 else
 	echo -e "\e[1;31m${bold}Errors occured ! Please see output above.${normal} \e[0m"
 fi
