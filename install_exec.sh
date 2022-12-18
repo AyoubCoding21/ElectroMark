2a3f9ee7fe872b39def55bb09294a0f224a9dd05
AyoubCoding21 <120122307+AyoubCoding21@users.noreply.github.com>
2022-12-18 10:45:01 +0100

    Update install_exec.sh

--- install_exec.sh
+++ install_exec.sh
@@ -12,7 +12,7 @@ then
 	rm ./build/ElectroMark
 fi
 echo -e "\e[1;32m${bold}Compilling and executing...${normal} \e[0m"
-clang++ ./src/main.cpp -o ./build/ElectroMark -mfp64 -mlong-double-128 -DFREEGLUT_STATIC -L/usr/lib64 -lGLEW -lGL -lX11 -lGLU -lglut
+clang++ ./src/main.cpp -o ./build/ElectroMark -mfp64 -mlong-double-128 $(pkg-config --cflags --libs glut glu glew)
 if [ -f ./build/ElectroMark ]
 then
 	chmod u+x ./build/ElectroMark && ./build/ElectroMark
