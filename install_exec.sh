2c4c141ed9f920fe33632b6b04c90dacc6ad4302
Ayoub Coding <120122307+AyoubCoding21@users.noreply.github.com>
2022-12-24 13:12:05 +0000

    Update install_exec.sh

--- install_exec.sh
+++ install_exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo -e "\e[1;32m${bold}Installing needed packages...${normal} \e[0m" && sudo apt -y install clang libglut-dev libglew-dev bash ld pkgconf &> /dev/null
-sleep 1s
+echo -e "\e[1;32m${bold}Installing needed packages...\e[0m" && sudo apt -y install bash clang libglut-dev libglew-dev pkgconf &> /dev/null
+sleep 5s
 if [ -d build ]
 then
 	sleep 1s
@@ -11,11 +11,13 @@ if [ -f ./build/ElectroMark ]
 then
 	rm ./build/ElectroMark
 fi
-echo -e "\e[1;32m${bold}Compilling and executing...${normal} \e[0m"
+echo -e "\e[1;32m${bold}Compilling and executing...\e[0m"
 clang++ ./src/main.cpp -o ./build/ElectroMark -mfp64 -mlong-double-128 $(pkg-config --cflags --libs glut glu glew)
 if [ -f ./build/ElectroMark ]
 then
-	chmod u+x ./build/ElectroMark && ./build/ElectroMark
+	chmod u+w+x ./build/ElectroMark && chmod 777 ./build/ElectroMark && ./build/ElectroMark
 else
-	echo -e "\e[1;31m${bold}Errors occured ! Please see output above.${normal} \e[0m"
+	echo -e "\e[1;31m${bold}Errors occured ! Please see output above.\e[0m"
 fi
+
+
